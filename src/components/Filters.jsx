f00ee16b355202511450e0235346a69daf81995f
kamser <keylor.morataya@gmail.com>
2026-05-23 23:14:30 -0700

    Added a visual element to help with the price filter to be more visual and added the function to handle the change

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -1,15 +1,25 @@
+import {useState} from 'react'
 import './filters.css'
 export function Filters(){
+
+    const [minPrice, setMinPrice] = useState(0)
+
+    const handleChangeMinPrice = (event) => {
+        setMinPrice(event.target.value)
+    }
+
     return (
         <section className="filters">
             <div>
-                <label htmlFor="price">Price</label>
+                <label htmlFor="price">Precio a partir de: </label>
                 <input 
                 type="range"
                 id="price"
                 min='0'
                 max='1000'
+                onChange={handleChangeMinPrice}
                  />
+                 <span>{minPrice}</span>
             </div>
 
             <div>
