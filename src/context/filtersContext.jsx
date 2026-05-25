24b9878db5d70033edebb5899961578895b56964
kamser <keylor.morataya@gmail.com>
2026-05-25 00:47:28 -0700

    Create a static context provider

--- src/context/filtersContext.jsx
+++ src/context/filtersContext.jsx
@@ -1,3 +1,18 @@
 import { createContext } from "react";
 
 export const FiltersContext = createContext()
+
+export function FiltersProvider ({children}){
+    return (
+        <FiltersContext.Provider
+            value={
+                {
+                    category: 'all',
+                    minPrice: 0
+                }
+            }
+        >
+            {children}
+        </FiltersContext.Provider>
+    )
+}
