f3effcaff33cb5f15383273957fa04d0cdf64609
kamser <keylor.morataya@gmail.com>
2026-05-25 14:27:08 -0700

    Create the basic cart ui and added to the App component

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import {products as initialProducts} from './mock/products.json'
 import { Products } from './components/Products.jsx'
 import { Header } from './components/Header.jsx'
 import { Footer } from './components/Footer.jsx'
+import { Cart } from './components/Cart.jsx'
 
 
 function App() {
@@ -16,6 +17,7 @@ function App() {
   return (
     <>
       <Header/>
+      <Cart/>
       <Products products={filteredProducts}></Products>
       <Footer />
     </>
---
+++ src/components/Cart.jsx
@@ -0,0 +1,35 @@
+import { useId } from "react";
+import { CartIcon, ClearCartIcon } from "./Icons";
+
+export function Cart(){
+    const cartCheckboxId = useId()
+    return (
+        <>
+            <label className="cart-button" htmlFor={cartCheckboxId}>
+                <CartIcon/>
+            </label>
+            <input id={cartCheckboxId} type="checkbox" hidden />
+
+            <aside className="cart">
+                <ul>
+                    <li>
+                        <img src="https://cdn.dummyjson.com/product-images/beauty/essence-mascara-lash-princess/thumbnail.webp" alt="Beauty product" />
+                        <div>
+                            <strong>Marcara lash</strong> - $100
+                        </div>
+
+                        <footer>
+                            <small>
+                                Qty: 1
+                            </small>'
+                            <button>+</button>
+                        </footer>
+                    </li>
+                </ul>
+                <button>
+                    <ClearCartIcon/>
+                </button>
+            </aside>
+        </>
+    )
+}
