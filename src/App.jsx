86ec0f341808a8e950a7f664d0c62defef5989fe
kamser <keylor.morataya@gmail.com>
2026-05-25 00:30:43 -0700

    Created a custom hook to handle the filter logic and use the filter in the App component

--- src/App.jsx
+++ src/App.jsx
@@ -3,24 +3,12 @@ import { useState } from 'react'
 import {products as initialProducts} from './mock/products.json'
 import { Products } from './components/Products.jsx'
 import { Header } from './components/Header.jsx'
+import { useFilters } from './hooks/useFilters.jsx'
 
 function App() {
 
   const [products, setProducts] = useState(initialProducts)
-  const [filters, setFilters] = useState({
-    category: 'all',
-    minPrice: 100
-  })
-
-  const filterProducts = (products) => {
-    return products.filter(product => {
-      return(
-        product.price >= filters.minPrice
-        &&
-        (filters.category === 'all' || product.category === filters.category)
-      )
-    })
-  }
+  const {filterProducts, setFilters} = useFilters({products})
 
   const filteredProducts = filterProducts(products)
 
