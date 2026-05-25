b0f0125f7127ab5752b92afbc021af7a4103204b
kamser <keylor.morataya@gmail.com>
2026-05-25 14:50:25 -0700

    Created the function to update the cart with items or increment the quantity if the product was already added

--- src/context/cartContext.jsx
+++ src/context/cartContext.jsx
@@ -5,6 +5,32 @@ export const CartContext = createContext()
 export function CartProvider({children}){
     const [cart, setCart] = useState([])
 
+    const addToCart = (product) => {
+        const productInCartIndex = cart.findIndex((item) => item.id === product.id)
+
+        if(productInCartIndex >= 0){
+            const updatedCart = structuredClone(cart)
+            updatedCart[productInCartIndex].quantity += 1
+            return setCart(updatedCart)
+        }
+
+        setCart(prevState => (
+            [
+                ...prevState,
+                {
+                    ...product,
+                    quantity: 1
+                }
+            ]
+        ))
+
+
+    }
+
+    const clearCart = () => {
+        setCart([])
+    }
+
     return(
         <CartContext.Provider
             value={{
