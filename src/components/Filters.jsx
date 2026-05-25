223f62590ac474bfecf26cb45b0498d889d89e86
kamser <keylor.morataya@gmail.com>
2026-05-25 00:15:14 -0700

    Added the hook useId to identify elements with uniqueness for the filters

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -1,8 +1,10 @@
-import {useState} from 'react'
+import {useState, useId} from 'react'
 import './filters.css'
 export function Filters({onChange}){
 
     const [minPrice, setMinPrice] = useState(0)
+    const minPriceFilterId = useId()
+    const categoryFilterId = useId()
 
     const handleChangeMinPrice = (event) => {
         setMinPrice(event.target.value)
@@ -24,10 +26,10 @@ export function Filters({onChange}){
     return (
         <section className="filters">
             <div>
-                <label htmlFor="price">Precio a partir de: </label>
+                <label htmlFor={minPriceFilterId}>Precio a partir de: </label>
                 <input 
                 type="range"
-                id="price"
+                id={minPriceFilterId}
                 min='0'
                 max='1000'
                 onChange={handleChangeMinPrice}
@@ -36,8 +38,8 @@ export function Filters({onChange}){
             </div>
 
             <div>
-                <label htmlFor="category">Categoria</label>
-                <select id="category" onChange={handleChangeCategory}>
+                <label htmlFor={categoryFilterId}>Categoria</label>
+                <select id={categoryFilterId} onChange={handleChangeCategory}>
                     <option value="all">Todas</option>
                     <option value="beauty">Belleza</option>
                     <option value="groceries">Comida</option>
