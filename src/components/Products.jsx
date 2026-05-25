c5897b77f4f259c79a2145acf79b536ed84d95e7
kamser <keylor.morataya@gmail.com>
2026-05-25 15:40:21 -0700

    Added the logic to change the button icon if the item is on teh cart or if it is not

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,27 +1,43 @@
-import { AddToCartIcon } from "./Icons"
+import { AddToCartIcon, RemoveFromCartIcon } from "./Icons"
 import './products.css'
 import { useCart } from "../hooks/useCart.jsx"
 
 export function Products({products}){
-    const {addToCart} = useCart()
+    const {cart, addToCart} = useCart()
+
+    const checkProductInCart = (id) =>{
+        return cart.find((item) => item.id === id)
+               ? true
+               : false 
+    }
 
     return(
         <main className="products">
             <ul>
                 {
-                    products.slice(0, 10).map((product) => 
-                        (
+                    products.slice(0, 10).map((product) => {
+                        const isProductInCart = checkProductInCart(product.id)
+
+                        return (
                             <li key={product.id}>
                                 <img src={product.thumbnail} alt={product.description} />
                                 <div>
                                     <p>{product.title}</p>
                                     <p>{product.price}</p>
                                 </div>
-                                <button onClick={() => addToCart(product)}>
-                                    <AddToCartIcon />
+                                <button 
+                                    onClick={() => {
+                                        addToCart(product)
+                                        }}>
+                                    {
+                                        isProductInCart
+                                            ? <RemoveFromCartIcon />
+                                            : <AddToCartIcon />
+                                    }
                                 </button>
                             </li>
-                        )
+                            )
+                        }
                     )
                 }
             </ul>
