5f9bf4694458e4a87b85803bf03231a231bdc739
kamser <keylor.morataya@gmail.com>
2026-05-25 18:51:46 -0700

    Create the reducer function, the initial state and the addToCart function for the reducer

--- src/context/cartContext.jsx
+++ src/context/cartContext.jsx
@@ -2,30 +2,56 @@ import { createContext, useState } from "react";
 
 export const CartContext = createContext()
 
+const initialState = []
+
+const reducer = (state, action) => {
+    const {type, payload} = action
+    switch(type){
+        case 'ADD_TO_CART': {
+            const {id} = payload
+            const productInCartIndex = state.findIndex((item) => item.id === id)
+
+            if(productInCartIndex >= 0){
+                const newCart = structuredClone(state)
+                newCart[productInCartIndex].quantity += 1
+                return newCart
+            }
+
+            return [
+                    ...state,
+                    {
+                        ...payload,
+                        quantity: 1
+                    }
+                ]
+        }
+        case 'REMOVE_FROM_CART': {}
+        case 'CLEAR_CART': {}
+    }
+}
+
 export function CartProvider({children}){
     const [cart, setCart] = useState([])
 
-    const addToCart = (product) => {
-        const productInCartIndex = cart.findIndex((item) => item.id === product.id)
+    // const addToCart = (product) => {
+    //     const productInCartIndex = cart.findIndex((item) => item.id === product.id)
 
-        if(productInCartIndex >= 0){
-            const updatedCart = structuredClone(cart)
-            updatedCart[productInCartIndex].quantity += 1
-            return setCart(updatedCart)
-        }
+    //     if(productInCartIndex >= 0){
+    //         const updatedCart = structuredClone(cart)
+    //         updatedCart[productInCartIndex].quantity += 1
+    //         return setCart(updatedCart)
+    //     }
 
-        console.log(product)
-
-        setCart(prevState => (
-            [
-                ...prevState,
-                {
-                    ...product,
-                    quantity: 1
-                }
-            ]
-        ))
-    }
+    //     setCart(prevState => (
+    //         [
+    //             ...prevState,
+    //             {
+    //                 ...product,
+    //                 quantity: 1
+    //             }
+    //         ]
+    //     ))
+    // }
 
     const removeFromCart = (product) => {
         const productInCartIndex = cart.findIndex(item => item.id === product.id)
