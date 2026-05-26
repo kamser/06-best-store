fc2bd5921ca79b548b8dcacc80656d2033981f83
kamser <keylor.morataya@gmail.com>
2026-05-25 19:34:56 -0700

    Added the functionality to persist the state of the store after refersh

--- src/reducer/cart.jsx
+++ src/reducer/cart.jsx
@@ -1,34 +1,44 @@
 import { ACTION_TYPE } from "./cartActionType.jsx"
 
-export const initialState = []
+export const initialState = JSON.parse(window.localStorage.getItem('cart')) || []
+
+export const updateLocalStorage = state => {
+    window.localStorage.setItem('cart', JSON.stringify(state))
+}
 
 export const reducer = (state, action) => {
     const {actionType, payload} = action
-    const {id} = payload
+    
     switch(actionType){
         case ACTION_TYPE.ADD_TO_CART: {
+            const {id} = payload
             const productInCartIndex = state.findIndex((item) => item.id === id)
 
             if(productInCartIndex >= 0){
                 const newCart = structuredClone(state)
                 newCart[productInCartIndex].quantity += 1
+                updateLocalStorage(newCart)
                 return newCart
             }
 
-            return [
-                    ...state,
-                    {
-                        ...payload,
-                        quantity: 1
-                    }
-                ]
+            const newCart = [
+                                ...state,
+                                {
+                                    ...payload,
+                                    quantity: 1
+                                }
+                            ]
+            updateLocalStorage(newCart)
+            return newCart
         }
         case ACTION_TYPE.REMOVE_FROM_CART: {
+            const {id} = payload
             const productInCartIndex = state.findIndex(item => item.id === id)
 
             if(productInCartIndex >= 0){
                 const newCart = structuredClone(state)
                 newCart.splice(productInCartIndex, 1)
+                updateLocalStorage(newCart)
                 return [
                     ...newCart
                 ]
@@ -36,7 +46,8 @@ export const reducer = (state, action) => {
             break;
         }
         case ACTION_TYPE.CLEAR_CART: {
-            return initialState
+            updateLocalStorage(initialState)
+            return []
         }
         default: {
             return []
